e805b9b940375e39e512f0eca5a3dca739992806
kshashi183 <shashidhar.k@ahana.co.in>
2023-04-27 23:07:12 +0530

    student management system

---
+++ src/main/java/net/javaguides/sms/StudentManagementSystemApplication.java
@@ -0,0 +1,40 @@
+package net.javaguides.sms;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import net.javaguides.sms.entity.Student;
+import net.javaguides.sms.repository.StudentRepository;
+
+@SpringBootApplication
+public class StudentManagementSystemApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+//		Student student1=new Student("ramesh", "fadtare", "ramesh@gmail.com");
+//		studentRepository.save(student1);
+//		
+//		Student student2=new Student("sanjay", "jadhva", "sanjay@gmail.com");
+//		studentRepository.save(student2);
+//		
+//		Student student3=new Student("tony", "stark", "tony@gmail.com");
+//		studentRepository.save(student3);
+		
+//		Student student4=new Student("Raj", "kumar", "Raj@gmail.com");
+//	studentRepository.save(student4);
+//		
+//		Student student5=new Student("rohit", "sharma", "rohit@gmail.com");
+//		studentRepository.save(student5);
+	}
+
+}
